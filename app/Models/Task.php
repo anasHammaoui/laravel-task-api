1d9cc9a0c7c097a12f981a87248cd5209c9e5744
anashm <anashammaoui07@gmail.com>
2025-02-25 21:42:02 +0000

    AR-1 complete models and migrations

---
+++ app/Models/Task.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /** @use HasFactory<\Database\Factories\TaskFactory> */
+    use HasFactory;
+    protected $timestamps = null;
+    protected $fillable = ['title','description','due_date','user_id'];
+}
